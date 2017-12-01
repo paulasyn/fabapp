9340a7bb375fe0a97b7df1ff9600bfd90a554cf4
Zach4550 <Zachariah.boone@mavs.uta.edu>
2017-11-30 23:53:48 -0600

    Merging changes from paul with personal progress

--- manageUsers/createUser.php
+++ manageUsers/createUser.php
@@ -15,9 +15,9 @@ if(isset($_SESSION['popup'])){
     if($_SESSION['popup'] == "Success"){ # User added successfully
         echo "<script type='text/javascript'> window.onload = function(){goModal('Success','The user was successfully added.', false)}</script>";
     } elseif($_SESSION['popup'] == ""){# Check if the pop up message is empty. This only happens if the submit button was hit but nothing was added for the pop up message. This is unexpected behavior.
-        echo "<script type='text/javascript'> window.onload = function(){goModal('Unknown Error','An unknown error occured while processing the request.', false)}</script>";
+        echo "<script type='text/javascript'> window.onload = function(){goModal('Unknown Error','An unknown error occured while processing the request.', true)}</script>";
     } else {# An input error occured. Display what error(s) occured in a pop up.
-        echo "<script type='text/javascript'> window.onload = function(){goModal('Input Error','" . $_SESSION['popup'] . "', false)}</script>";
+        echo "<script type='text/javascript'> window.onload = function(){goModal('Input Error','" . $_SESSION['popup'] . "', true)}</script>";
     }
     /*Unset the error value in case of refresh.*/
     unset($_SESSION['popup']);
@@ -191,12 +191,21 @@ else {echo "<!-- The pop up window value was not set. -->";}
                         <?php
                         $error_message = "";
                         if (isset($_POST['submit'])){
-    
-                            $r_id = mysqli_real_escape_string($mysqli, $_POST['r_id']);
-                            $operator = mysqli_real_escape_string($mysqli, $_POST['operator']);
+							$userType = $_POST['userRadio'];#To check the value of the radio button
+                            $r_id = mysqli_real_escape_string($mysqli, $_POST['r_id']); #Needed for both off and on campus
+                            $operator = mysqli_real_escape_string($mysqli, $_POST['operator']); #Needed for both off and on campus
                             $icon = mysqli_real_escape_string($mysqli, $_POST['icon']);
-                            $notes = mysqli_real_escape_string($mysqli, $_POST['notes']);
-
+                            $notes = mysqli_real_escape_string($mysqli, $_POST['notes']); #Needed for both off and on campus
+							#Above, values for on campus. Below, values for off campus.
+							$zip = mysqli_real_escape_string($mysqli, $_POST['zip']);#Zip zip
+							$state = mysqli_real_escape_string($mysqli, $_POST['state']);#State state
+							$city = mysqli_real_escape_string($mysqli, $_POST['city']);#City city 
+							$address = mysqli_real_escape_string($mysqli, $_POST['address']);#Street address
+							$email = mysqli_real_escape_string($mysqli, $_POST['email']);#email email
+							$phone = mysqli_real_escape_string($mysqli, $_POST['phone']);#Phone phone
+							$lname = mysqli_real_escape_string($mysqli, $_POST['lname']);#Last Name lname
+							$fname = mysqli_real_escape_string($mysqli, $_POST['fname']);#First Name fname
+							
                             $_SESSION['popup'] = "";
                             $input_error = false;
     
@@ -211,7 +220,12 @@ else {echo "<!-- The pop up window value was not set. -->";}
                                 $input_error = true;
                             }
                             #Make sure you're not entering a user with a repat 1000s number
-                            $sql = "SELECT * FROM users WHERE operator = '$operator'";
+							if ($userType == "0"){#on campus table operators
+								$sql = "SELECT * FROM users WHERE operator = '$operator'";
+							}
+							else{ #off campus table operators
+								$sql = "SELECT * FROM offcampus WHERE operator = '$operator'";
+							}
                             $result = mysqli_query($mysqli, $sql);
                             $resultCheck = mysqli_num_rows($result);
                                     
@@ -222,7 +236,12 @@ else {echo "<!-- The pop up window value was not set. -->";}
                             }
                             # If there was no input error, then the query should be formatted correctly.
                             if(!$input_error) {
-                                $sql = "INSERT INTO users (operator, r_id, icon, notes) VALUES ('$operator', '$r_id', '$icon', '$notes');";
+								if ($userType == "0"){#on campus table operators
+									$sql = "INSERT INTO users (operator, r_id, icon, notes) VALUES ('$operator', '$r_id', '$icon', '$notes');";
+								}
+								else{
+									$sql = "INSERT INTO offcampus (operator, fname, lname, phone, email, address, city, state, zip) VALUES ('$operator', '$fname', '$lname', '$phone','$email','$address','$city','$state','$zip');"; #offcampus table needs r_id icon and notes columns
+								}
                                 mysqli_query($mysqli, $sql);
                                 $_SESSION['popup'] = "Success";
                             }
